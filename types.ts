3f0ee9e572e4f837cd162b9cdc10d05267bf78c6
ty-mxm <165325425+ty-mxm@users.noreply.github.com>
2024-12-16 14:25:43 -0500

    ajustement design home page

--- types.ts
+++ types.ts
@@ -1,16 +1,15 @@
 export type RootStackParamList = {
-    Home: undefined;
-    EventDetails: { event: Event };
-    AddEvent: undefined;
-  };
-  
-  // Interface pour les événements
-  export interface Event {
-    id: string;
-    title: string;
-    location: string;
-    date: string;
-    time: string;
-    category: string;
-  }
-  
+  Home: undefined;
+  EventDetails: { event: Event };
+  AddEvent: undefined;
+};
+
+// Interface pour les événements
+export interface Event {
+  id: string;
+  title: string;
+  location: string;
+  date: string;
+  time: string;
+  category: string;
+}
